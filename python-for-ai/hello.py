d7e3aa752468c69380e51303afadcc6e3cbc2e20
Askhat Amankulov <askhat.0103@mail.ru>
2026-09-06 06:54:39 +0500

    Initial commit

---
+++ python-for-ai/hello.py
@@ -0,0 +1,9 @@
+
+
+class IBilim:
+    def __init__(self, name, grade):
+        self.name = name
+        self.grade = grade
+
+student1 = IBilim("Nurdos", '11B')
+print(student1.name, student1.grade)
